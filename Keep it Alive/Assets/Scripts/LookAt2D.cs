cd32b2b09078b6181e7629574cce8856675feb36
PierreBaudoin <pierre.baudoin@free.fr>
2020-04-19 18:32:34 +0200

    LookAt2D, à placer sur les Orbiters

---
+++ Keep it Alive/Assets/Scripts/LookAt2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt2D : MonoBehaviour
+{
+    public GameObject target;
+    void Update()
+    {
+        Vector3 diff = target.transform.position - transform.position;
+        diff.Normalize();
+ 
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
+}
