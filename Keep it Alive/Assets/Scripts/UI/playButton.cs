9673c8a7e68cbc0716283e77a9162f4a0fa75922
PierreBaudoin <pierre.baudoin@free.fr>
2020-04-19 17:56:39 +0200

    Menu (manque à lier les scenes)

---
+++ Keep it Alive/Assets/Scripts/UI/playButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playButton : MonoBehaviour
+{
+    public int GameSceneId;
+    public void OnClick(){
+        SceneManager.LoadScene(GameSceneId);
+    }
+}
