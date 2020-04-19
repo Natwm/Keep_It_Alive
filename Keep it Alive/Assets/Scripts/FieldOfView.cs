41707586c6e1828553ed5ac866cca4ddef796e46
natwPine <natw.mezza@hotmail.fr>
2020-04-19 16:49:27 +0200

    fov

--- Keep it Alive/Assets/Scripts/FieldOfView.cs
+++ Keep it Alive/Assets/Scripts/FieldOfView.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 public class FieldOfView : MonoBehaviour
 {
     #region Param
+    [Header ("Field Of View")]
     [SerializeField]private float viewRadius;
-
-    [SerializeField] private float delay;
 
     [Range (0,360)]
     [SerializeField] private float viewAngle;
 
+    [Range(0, 1)]
+    [SerializeField] private float meshResolution;
+
+    public MeshFilter viewMeshFilter;
+    private Mesh viewMesh;
+
+    [Space]
+    [Header ("Time Between two Raycast")]
+    [SerializeField] private float delay;
+
+    [Space]
+    [Header("Target")]
     [SerializeField] private LayerMask targetMask;
     [SerializeField] private LayerMask obstacleMask;
 
-    [SerializeField] private List<Transform> visibleTarget = new List<Transform>();
-    [SerializeField] private List<GameObject> visibleGameobject = new List<GameObject>();
+     private List<Transform> visibleTarget = new List<Transform>();
+     private List<GameObject> visibleGameobject = new List<GameObject>();
     #endregion
 
     #region UPDATE
     private void Start()
     {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+
+        viewMeshFilter.mesh = viewMesh;
+
         StartCoroutine("FindTargetWithDelay", delay);
     }
-    private void FixedUpdate()
+    private void Update()
     {
+        DrawFieldOfView();
     }
     #endregion
 
@@ -86,9 +103,74 @@ public class FieldOfView : MonoBehaviour
 
     #endregion
 
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoint = new List<Vector3>();
 
-    #region GETTER && SETTER
-    public float ViewAngle { get => viewAngle; set => viewAngle = value; }
+        for (int i = 0; i < stepCount; i++)
+        {
+            float angle = transform.eulerAngles.z - viewAngle/2 + stepAngleSize * i;
+            Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius,Color.red);
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoint.Add(newViewCast.point);
+        }
+        int vertexCount = viewPoint.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoint[i]);
+
+            if(i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+        viewMesh.Clear();
+
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+    
+    ViewCastInfo ViewCast (float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, dir,out hit, ViewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dist;
+        public float angle;
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dist, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dist = _dist;
+            angle = _angle;
+        }
+    }
+
+
+        #region GETTER && SETTER
+        public float ViewAngle { get => viewAngle; set => viewAngle = value; }
     public LayerMask TargetMask { get => targetMask; set => targetMask = value; }
     public List<Transform> VisibleTarget { get => visibleTarget; set => visibleTarget = value; }
     public float ViewRadius { get => viewRadius; set => viewRadius = value; }
